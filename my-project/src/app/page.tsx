56a493f3110934c073c63a62ae13ba11b7c05aed
Lucas Ligas <lucasligas15@gmail.com>
2025-01-09 13:29:32 +0100

    feat(app): landing responsivity

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -2,29 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen">
       <div className="p-10 items-center justify-items-center gap-16 ">
+              <div className="w-28 md:w-32">
               <Image
                 src="/šAIch..svg"
                 alt="ŠAICH"
                 width={150}
                 height={66}
-                sizes="(max-width: 768px) 100vw, 
-                      (max-width: 1200px) 50vw, 
-                      33vw"/>
+                layout="responsive"
+                />
+                </div>
       </div>
-      <div className="flex flex-row justify-center gap-10 mt-10">
-        <div className="bg-slate-600 w-[640px] h-[640px]"/>
-        <div>
-          <h1 className="mt-[-26px]">
-            Maturitná práca<br/> <span className="color">Lucasa Ligasa</span><br/> <span className="underline ">2025</span>
-          </h1>
-          <div className="flex flex-col gap-10 mt-16">
-            <button type="button"className="primary">Prihlásiť sa</button>
-            <button type="button"className="secondary">Zaregistrovať sa</button>
+      <div className="justify-center items-center flex flex-row w-full h-full">
+        <div className="flex flex-col xl:flex-row justify-center items-center gap-10">
+          <div className="w-[70%] sm:w-1/3 md:w-1/2 max-w-[450px] aspect-square rounded">
+            <Image 
+              width={5} 
+              height={5} 
+              layout="responsive" 
+              src="/chessboard.png"
+              alt="chessboard"
+              />
           </div>
+          <div>
+            <h1 className="text-center md:text-5xl xl:text-start text-4xl xl:text-7xl">
+              Maturitná práca<br/> <span className="color">Lucasa Ligasa</span><br/> <span className="underline ">2025</span>
+            </h1>
+            <div className="flex flex-col items-center xl:items-start gap-5 mt-10">
+              <button type="button"className="w-[90%] primary h-20 text-2xl">Prihlásiť sa</button>
+              <button type="button"className="w-[90%] secondary w-96 h-20 text-2xl">Zaregistrovať sa</button>
+            </div>
+          </div>
+          
         </div>
-        
       </div>
     </div>
   );
