eda62ab2ec24c7db3b2791d98bb72b1402fbd853
Lucas Ligas <lucasligas15@gmail.com>
2025-02-05 22:17:54 +0100

    feat(app): working login/register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,50 @@
-import Image from "next/image";
-import Link from 'next/link'
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    // Reset error message
+    setError("");
+
+    // Send data to the backend
+    try {
+      const response = await fetch("http://localhost:5001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Redirect to a protected page or dashboard
+        router.push("/opponentSelect");
+      } else {
+        if (data.error == "Invalid credentials") {
+          setError("Nesprávne meno alebo heslo");
+        } else {
+          setError(data.error || "Prihlásenie zlyhalo");
+        }
+      }
+    } catch (err) {
+      setError("Niečo sa pokazilo skúste to prosím neskô");
+      console.log(err);
+    }
+  };
+
   return (
     <div className="min-h-screen">
       <div className="p-10 items-center flex justify-center gap-16 ">
@@ -17,28 +59,55 @@ export default function Login() {
             />
           </div>
         </Link>
-
       </div>
       <div className="flex flex-col items-center mt-10">
-        <h1 className="text-3xl">
-          Prihlásenie
-        </h1>
-        <form className="flex flex-col gap-2 text-black">
+        <h1 className="text-3xl">Prihlásenie</h1>
+        <form
+          className="flex flex-col gap-2 text-black"
+          onSubmit={handleSubmit}
+        >
           <div className="flex flex-col w-[80vw] lg:w-[30vw]">
-            <label htmlFor="email"><span className="color font-semibold">Email</span></label>
-            <input id="email" type="email" className="h-10 rounded-xl px-3" placeholder="email@domain.com"></input>
+            <label htmlFor="input">
+              <span className="color font-semibold">Prezívka</span>
+            </label>
+            <input
+              id="nick"
+              type="input"
+              className="h-10 rounded-xl px-3"
+              placeholder="Šachista"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            ></input>
           </div>
           <div className="flex flex-col">
-            <label htmlFor="password"><span className="color font-semibold">Heslo</span></label>
-            <input id="password" type="password" className="h-10 rounded-xl px-3" placeholder="********"></input>
+            <label htmlFor="password">
+              <span className="color font-semibold">Heslo</span>
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="h-10 rounded-xl px-3"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            ></input>
           </div>
-          <button className="primary h-16 mt-5 text-white" type="submit">Prihlásiť sa</button>
+          <button className="primary h-16 mt-5 text-white" type="submit">
+            Prihlásiť sa
+          </button>
         </form>
-
-        <p className="mt-5 font-normal">Nemáte účet ? <Link href="/register"><span className="underline font-semibold text-[#FF5100]">Registrujte sa tu !</span></Link></p>
+        {error && <p style={{ color: "red", marginTop: "10px" }}>{error}</p>}
+        <p className="mt-5 font-normal">
+          Nemáte účet ?{" "}
+          <Link href="/register">
+            <span className="underline font-semibold text-[#FF5100]">
+              Registrujte sa tu !
+            </span>
+          </Link>
+        </p>
       </div>
-
-
     </div>
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,60 @@
+"use client"
+
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
+import { useRouter } from 'next/navigation'
+import { useState } from "react";
 
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter()
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    // Reset messages
+    setError("");
+
+    // Validate passwords match
+    if (password !== confirmPassword) {
+      setError("Heslá sa nezhodujú");
+      return;
+    }
+
+    // Send data to the backend
+    try {
+      const response = await fetch("http://localhost:5001/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password, confirmPassword }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setUsername("");
+        setPassword("");
+        setConfirmPassword("");
+        router.push("/opponentSelect")
+      } else {
+        setError(data.error || "Registrácia zlyhala");
+      }
+    } catch (err) {
+      setError("Niečo sa pokazilo :/ skúste to neskôr");
+      console.log(err);
+    }
+  };
+
   return (
     <div className="min-h-screen">
       <div className="p-10 items-center flex justify-center gap-16 ">
         <Link href="/">
-
           <div className="w-28 md:w-32">
             <Image
               src="/šAIch..svg"
@@ -18,32 +65,70 @@ export default function Register() {
             />
           </div>
         </Link>
-
       </div>
       <div className="flex flex-col items-center mt-10">
-        <h1 className="text-3xl">
-          Registrácia
-        </h1>
-        <form className="flex flex-col gap-2 text-black">
+        <h1 className="text-3xl">Registrácia</h1>
+        <form
+          className="flex flex-col gap-2 text-black"
+          onSubmit={handleSubmit}
+        >
           <div className="flex flex-col w-[80vw] lg:w-[30vw]">
-            <label htmlFor="email"><span className="color font-semibold">Email</span></label>
-            <input id="email" type="email" className="h-10 rounded-xl px-3" placeholder="email@domain.com"></input>
+            <label htmlFor="nick">
+              <span className="color font-semibold">Prezívka</span>
+            </label>
+            <input
+              id="nick"
+              type="input"
+              className="h-10 rounded-xl px-3"
+              placeholder="Šachista"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            ></input>
           </div>
           <div className="flex flex-col">
-            <label htmlFor="password"><span className="color font-semibold">Heslo</span></label>
-            <input id="password" type="password" className="h-10 rounded-xl px-3" placeholder="********"></input>
+            <label htmlFor="password">
+              <span className="color font-semibold">Heslo</span>
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="h-10 rounded-xl px-3"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            ></input>
           </div>
           <div className="flex flex-col">
-            <label htmlFor="password"><span className="color font-semibold">Potvrdenie hesla</span></label>
-            <input id="password" type="password" className="h-10 rounded-xl px-3" placeholder="********"></input>
+            <label htmlFor="password">
+              <span className="color font-semibold">Potvrdenie hesla</span>
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="h-10 rounded-xl px-3"
+              placeholder="********"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            ></input>
           </div>
-          <button className="primary h-16 mt-5 text-white" type="submit">Zaregistrovať sa</button>
+          <button className="primary h-16 mt-5 text-white" type="submit">
+            Zaregistrovať sa
+          </button>
         </form>
-
-        <p className="mt-5 font-normal">Máte účet ? <Link href="/login"><span className="underline font-semibold text-[#FF5100]">Prihláste sa tu !</span></Link></p>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <p className="mt-5 font-normal">
+          Máte účet ?{" "}
+          <Link href="/login">
+            <span className="underline font-semibold text-[#FF5100]">
+              Prihláste sa tu !
+            </span>
+          </Link>
+        </p>
+        
       </div>
-
-
     </div>
   );
 }
